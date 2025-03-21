687bc8b005f29ec1f2d5cbd5a8a6c97b4233ce32
Rhys-I-B <100147078+Rhys-I-B@users.noreply.github.com>
2025-03-21 15:12:14 +0000

    Added routed to react and additional php file

---
+++ app/Controllers/Routes.php
@@ -0,0 +1,26 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+
+use App\Controllers\News;
+use App\Controllers\Pages;
+use App\Controllers\React;
+
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/new', [News::class, 'new']);
+$routes->get('news/json', [News::class, 'json']);
+$routes->post('news', [News::class, 'create']);
+$routes->get('news/(:segment)', [News::class, 'show']);
+
+$routes->get('react', [React::class, 'index']);
+
+
+// MUST BE LAST
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
