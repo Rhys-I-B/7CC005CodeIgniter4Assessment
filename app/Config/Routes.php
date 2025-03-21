e8a4c513dc6d0bab815d0abc5a20785e9a0ecf88
Rhys-I-B <100147078+Rhys-I-B@users.noreply.github.com>
2025-03-21 13:38:17 +0000

    Made changes to add React files

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,11 +10,16 @@ $routes->get('/', 'Home::index');
 
 use App\Controllers\News;
 use App\Controllers\Pages;
+use App\Controllers\React;
 
 $routes->get('news', [News::class, 'index']);
 $routes->get('news/new', [News::class, 'new']);
 $routes->post('news', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'show']);
 
+$routes->get('react', [React::class, 'index']);
+
+
+// MUST BE LAST
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
